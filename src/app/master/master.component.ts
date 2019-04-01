ac6f55a8bc13522519cccc7651d5e178e15133c7
IanSani <iansani259@gmail.com>
2019-04-01 11:31:26 +0300

    Added logic for user search details

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-master',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
-  constructor() { }
+  user1;
+  repos;
+  userName: string;
+
+  constructor(public apiservice: ApiService, private router: Router) {
+
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
+  }
+
+  findUser() {
+    this.apiservice.updateUserName(this.userName);
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
