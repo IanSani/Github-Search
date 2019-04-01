814eaf638eecdff050a0d55ec2b1168075e25038
IanSani <iansani259@gmail.com>
2019-04-01 12:03:58 +0300

    edited app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +9,7 @@ import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { MasterComponent } from './master/master.component';
 import { RepoComponent } from './repo/repo.component';
-
+import {StrikethroughDirective} from './strikethrough.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,12 +17,15 @@ import { RepoComponent } from './repo/repo.component';
     DateCountPipe,
     MasterComponent,
     RepoComponent
+    StrikethroughDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RoutingModule,
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
